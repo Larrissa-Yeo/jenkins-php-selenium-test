afa785319451c2dc4c20e392f3e0773d35af82af
Larrissa Yeo <yxtlar@gmail.com>
2024-07-19 00:24:06 +0800

    Update AppTest.java

--- src/test/java/mycompany/app/AppTest.java
+++ src/test/java/mycompany/app/AppTest.java
@@ -23,7 +23,7 @@ public class AppTest
 {
 	WebDriver driver; 
 	WebDriverWait wait; 
-	String url = "http://169.254.83.107";
+	String url = "http://192.168.10.144";
 	String validEmail = "user@example.com";
 	String validPassword = "password1234";
 	String invalidEmail = "none@example.com";
